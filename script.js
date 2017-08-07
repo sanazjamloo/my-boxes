4cb353032dcc8f7fe924586fa5b6bb8a9ab35c7a
Sanaz <sanaz.jamloo@gmail.com>
2017-08-07 01:07:17 -0700

    Create JS function

---
+++ script.js
@@ -0,0 +1,22 @@
+var colors = [
+  '#C94C24', //orange
+  '#268BD2', //blue
+  '#C4226F', //pink
+  '#859835', //lime
+  '#6D73C2', //purple
+  '#37A198', //green
+  '#DA3637', //red
+  '#F0ADE', //yellow
+];
+
+function makeBoxes (howMany) {
+  var colorAmt = colors.length;
+  var currColor = 0;
+  for (var i = 0; i < howMany; i++) {
+    if (currColor === colorAmt-1) {
+      currColor = 0;
+    } else {
+      currColor++;
+    }
+  }
+}
