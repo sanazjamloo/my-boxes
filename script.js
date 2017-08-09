fdf2730ebe00b3974ad9ea53fecc663bb0c24346
Sanaz <sanaz.jamloo@gmail.com>
2017-08-08 17:10:03 -0700

    Create new element

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ var colors = [
 function makeBoxes (howMany) {
   var colorAmt = colors.length;
   var currColor = 0;
+  var myElement;
+  var myNode = document.querySelector('.boxes');
   for (var i = 0; i < howMany; i++) {
+
+    myElement = document.createElement('div');
+    myElement.className = 'box';
+    myElement.style = 'background-color:' + colors[currColor];
+    myNode.appendChild(myElement);
     if (currColor === colorAmt-1) {
       currColor = 0;
     } else {
@@ -20,3 +27,6 @@ function makeBoxes (howMany) {
     }
   }
 }
+
+
+makeBoxes(34);
